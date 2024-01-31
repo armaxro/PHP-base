8b707e3c16985e8712352d4b7fb6d9ae8260652d
armaxro <arnaldomrojas@outlook.com>
2024-01-31 14:49:04 +0100

    not finished

--- stagiaires/Maxi/29-exemples-fonctions.php
+++ stagiaires/Maxi/29-exemples-fonctions.php
@@ -1,1 +1,40 @@
 <?php
+
+require "array.php";
+
+
+// preocedure
+function helloWorld(){
+    echo "hello Wolrd!";
+}
+
+helloWorld();
+
+//fonction avec retour
+function myHelloWorld(){
+    //envoit une valeur et s'arrete
+    return "Hello World";
+}
+
+echo myHelloWorld();
+
+
+
+
+function upperArray(array $tab): string 
+{
+    $text ="";
+    foreach($tab as $value){
+        $value .= strtoupper($value). "<br>";
+        $txt  .=upperArray($value);
+    }else{
+        
+    }
+    }
+    return  $txt;
+}
+
+echo upperArray("coucou");
+echo "<br>";
+echo upperArray($classe);
+
